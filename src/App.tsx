829ebe3bf1ecb32e3c6552c0cfd27766d42123de
Kacper Golinski <kacper.golinski@gmail.com>
2019-03-03 17:24:36 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Text, View } from 'react-native'
+
+export function App() {
+  return (
+    <View style={{ marginTop: 50 }}>
+      <Text>Hello World!</Text>
+    </View>
+  )
+}
